2e190092e794ec505d7fd4644c3817d25dc62196
Harry Kim <hkim@Harrys-MacBook-Air.local>
2023-07-19 12:53:03 -0700

    Remove node modules

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,22 +1,7 @@
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-const root = resolve(__dirname, 'src')
-const outDir = resolve(__dirname, 'dist')
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  root,
   plugins: [react()],
-  build: {
-    outDir,
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: resolve(root, 'index.html'),
-        generate: resolve(root, 'generate', 'index.html'),
-      }
-    }
-  }
 })
